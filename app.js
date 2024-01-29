f4bbd963ad1ee0e2fedbcbbc9fe80912fee30447
omta <omtadayma2001@gmail.com>
2024-01-29 10:25:56 +0530

    first commit

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const cors = require("cors");
 const router = require("./Routes/router");
 //db connect
 require("./models/db");
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 8000
 
 app.use(cors());
 app.use(express.json());
